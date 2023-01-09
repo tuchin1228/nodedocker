7ebcbd6fb5c202b2bff28309c54cfc0cb855f40b
tuchin1228 <jhinwei1228@gmail.com>
2023-01-09 13:17:24 +0800

    add 5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express develop 333333' });
+  res.render('index', { title: 'Express develop 55555' });
 });
 
 module.exports = router;
