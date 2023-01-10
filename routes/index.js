554129998b545e5746d7d3197ad90d094158b9e2
tuchin1228 <jhinwei1228@gmail.com>
2023-01-10 14:11:18 +0800

    add 8 and edit node

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express develop 66666' });
+  res.render('index', { title: 'Express develop 88888' });
 });
 
 module.exports = router;
