b9bfbfeacd2ff72d140bfeaee34b311ed9a96407
tuchin1228 <jhinwei1228@gmail.com>
2023-01-18 11:02:45 +0800

    add time

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express develop 88888' });
+  res.render('index', { title: 'Express develop 0118 11:02' });
 });
 
 module.exports = router;
