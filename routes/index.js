f695b3cec50920575d7927d37e63b425197c5503
tuchin1228 <jhinwei1228@gmail.com>
2023-01-18 11:42:50 +0800

    upadte time

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express develop 0118 11:02' });
+  res.render('index', { title: 'Express develop 0118 11:42' });
 });
 
 module.exports = router;
