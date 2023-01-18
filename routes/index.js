252703dfc713c70545bff0faa117b852a2ba2360
tuchin1228 <jhinwei1228@gmail.com>
2023-01-18 15:07:26 +0800

    update time 1507

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express develop 0118 14:48' });
+  res.render('index', { title: 'Express develop 0118 15:07' });
 });
 
 module.exports = router;
